c2fdd2652a78f619ba033ade3dc6dd07e9bc66b1
anika2008 <anikamishra.princess@gmail.com>
2023-01-21 11:38:19 +0530

    hi

---
+++ game.cs
@@ -0,0 +1,40 @@
+using System;
+using System. Collection.Generic;
+using System. CompoentModel;
+using System. Data;
+using System. Drawing;
+using System. Linq;
+using System. Text;
+using System. Threading. Task;
+using System. WIndows. Form;
+
+namespace KnockOut
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void enemyMoveEvent (object sender, Event Args e)
+        {
+
+        }
+
+        private void enemyPunchEvent (object sender, Event Args e)
+        {
+
+        }
+
+        private void keyisdown (object sender, KeyEvent Args e)
+        {
+
+        }
+
+        private void keyisup (object sender, KeyEvent Args e)
+        {
+            
+        }
+    }
+}
