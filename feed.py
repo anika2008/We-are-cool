4b0a7fab6718ee907d2e72676f4ff842f07a6613
anika2008 <123225586+anika2008@users.noreply.github.com>
2023-01-21 19:07:50 +0530

    Update feed.py

--- feed.py
+++ feed.py
@@ -1,3 +1,22 @@
+import time
+
 print("This is the feed back page")
-input("Please enter your feedback: ")
+rate = int(input("Rate the game out of 10: "))
+if (rate >= 5):
+  print("Thank you so much")
+  wrong = "0"
+else:
+  wrong = input("What went wrong: ")
+fb = input("Please write overall feedback: ")
 print("Thank you for your feed back! We'll take it and improve our game. Have a nice day")
+
+if (rate >= 5):
+  time.sleep(7)
+  print(" ")
+  print(fb)
+else: 
+  time.sleep(7)
+  print(" ")
+  print(wrong)
+  print(" ")
+  print(fb)
